3ef6b63b530a4a60a056e961665c56755c7f7b8d
JIHYEPYO <pyojihye95@gmail.com>
2017-08-21 17:01:03 +0900

    Program Finished

---
+++ JsonParsing/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace JsonParsing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.originToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sortingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tAGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 62);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowTemplate.Height = 23;
+            this.dataGridView.Size = new System.Drawing.Size(1230, 667);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuStrip1.Font = new System.Drawing.Font("맑은 고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.originToolStripMenuItem,
+            this.sortingToolStripMenuItem,
+            this.tAGToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1254, 45);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // originToolStripMenuItem
+            // 
+            this.originToolStripMenuItem.Name = "originToolStripMenuItem";
+            this.originToolStripMenuItem.Size = new System.Drawing.Size(108, 41);
+            this.originToolStripMenuItem.Text = "Origin";
+            this.originToolStripMenuItem.Click += new System.EventHandler(this.originToolStripMenuItem_Click);
+            // 
+            // sortingToolStripMenuItem
+            // 
+            this.sortingToolStripMenuItem.Name = "sortingToolStripMenuItem";
+            this.sortingToolStripMenuItem.Size = new System.Drawing.Size(122, 41);
+            this.sortingToolStripMenuItem.Text = "Sorting";
+            this.sortingToolStripMenuItem.Click += new System.EventHandler(this.sortingToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(798, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(267, 29);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "---Select User\'s Name---";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tAGToolStripMenuItem
+            // 
+            this.tAGToolStripMenuItem.Name = "tAGToolStripMenuItem";
+            this.tAGToolStripMenuItem.Size = new System.Drawing.Size(95, 41);
+            this.tAGToolStripMenuItem.Text = "TAG?";
+            this.tAGToolStripMenuItem.Click += new System.EventHandler(this.tAGToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button1.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(1088, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Save as Excel";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1254, 749);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Json Parsing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem originToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sortingToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ToolStripMenuItem tAGToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ JsonParsing/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Newtonsoft.Json;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Excel = Microsoft.Office.Interop.Excel;
+using ClosedXML.Excel;
+
+namespace JsonParsing
+{
+    public partial class Form1 : Form
+    {
+        static string fileName;
+        static string fileFullName;
+        static string filePath;
+        static List<string> name;
+        DataTable dataTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowFileOpenDialog();
+            ConvertJsonToDataTable();
+            comboBox1.Visible = false;
+        }
+
+        private static void ShowFileOpenDialog()
+        {
+            //파일오픈창 생성 및 설정
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Json 파일 선택";
+            ofd.Filter = "json 파일 (*.json) | *.json; | 모든 파일 (*.*) | *.*";
+
+            //파일 오픈창 로드
+            DialogResult dr = ofd.ShowDialog();
+
+            //OK버튼 클릭시
+            if (dr == DialogResult.OK)
+            {
+                fileName = ofd.SafeFileName;
+                fileFullName = ofd.FileName;
+                filePath = fileFullName.Replace(fileName, "");
+            }
+        }
+
+        private void ConvertJsonToDataTable()
+        {
+            name = new List<string>();
+
+            try
+            {
+                string jsonString = File.ReadAllText(fileFullName);
+
+                if (!String.IsNullOrWhiteSpace(jsonString))
+                {
+                    dynamic dynObj = JsonConvert.DeserializeObject(jsonString);
+
+                    dataTable = new DataTable();
+                    dataTable.Columns.Add("num", typeof(string));
+                    dataTable.Columns.Add("userName", typeof(string));
+                    dataTable.Columns.Add("time", typeof(string));
+                    dataTable.Columns.Add("tag", typeof(string));
+                    dataTable.Columns.Add("filename", typeof(string));
+                    dataTable.Columns.Add("location", typeof(string));
+                    dataTable.Columns.Add("mode", typeof(string));
+                    dataTable.Columns.Add("text", typeof(string));
+                    dataTable.Columns.Add("wpm", typeof(string));
+                    dataTable.Columns.Add("delete", typeof(string));
+                    dataTable.Columns.Add("gap", typeof(string));
+                    dataTable.Columns.Add("activityName", typeof(string));
+                    dataTable.Columns.Add("pushButton", typeof(string));
+                    dataTable.Columns.Add("value", typeof(string));
+
+                    AddRows(dynObj.ButtonMenu, "ButtonMenu");
+                    AddRows(dynObj.ButtonTraining, "ButtonTraining");
+                    AddRows(dynObj.ButtonViewer, "ButtonViewer");
+                    AddRows(dynObj.File, "File");
+                    AddRows(dynObj.Mode, "Mode");
+                    AddRows(dynObj.Training, "Training");
+                    AddRows(dynObj.Viewer, "Viewer");
+
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        dataGridView.DataSource = dataTable;
+                        dataGridView.Columns["num"].Visible = false;
+                        dataGridView.Columns["userName"].HeaderText = "User Name";
+                        dataGridView.Columns["time"].HeaderText = "Date";
+                        dataGridView.Columns["tag"].HeaderText = "TAG";
+                        dataGridView.Columns["filename"].HeaderText = "File Name";
+                        dataGridView.Columns["location"].HeaderText = "Location";
+                        dataGridView.Columns["mode"].HeaderText = "Mode";
+                        dataGridView.Columns["text"].HeaderText = "Text";
+                        dataGridView.Columns["wpm"].HeaderText = "WPM";
+                        dataGridView.Columns["delete"].HeaderText = "Delete";
+                        dataGridView.Columns["gap"].HeaderText = "Gap";
+                        dataGridView.Columns["activityName"].HeaderText = "Activity Name";
+                        dataGridView.Columns["pushButton"].HeaderText = "Push Button";
+                        dataGridView.Columns["value"].HeaderText = "Value";
+
+                        dataTable.DefaultView.Sort = "time";
+                        dataGridView.ClearSelection();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void SortTable()
+        {
+            try
+            {
+                string jsonString = File.ReadAllText(fileFullName);
+
+                if (!String.IsNullOrWhiteSpace(jsonString))
+                {
+                    dynamic dynObj = JsonConvert.DeserializeObject(jsonString);
+
+                    dataTable = new DataTable();
+                    dataTable.Columns.Add("num", typeof(string));
+                    dataTable.Columns.Add("userName", typeof(string));
+                    dataTable.Columns.Add("time", typeof(string));
+                    dataTable.Columns.Add("tag", typeof(string));
+                    dataTable.Columns.Add("filename", typeof(string));
+                    dataTable.Columns.Add("location", typeof(string));
+                    dataTable.Columns.Add("mode", typeof(string));
+                    dataTable.Columns.Add("text", typeof(string));
+                    dataTable.Columns.Add("wpm", typeof(string));
+                    dataTable.Columns.Add("delete", typeof(string));
+                    dataTable.Columns.Add("gap", typeof(string));
+                    dataTable.Columns.Add("activityName", typeof(string));
+                    dataTable.Columns.Add("pushButton", typeof(string));
+                    dataTable.Columns.Add("value", typeof(string));
+
+                    SortAddRows(dynObj.ButtonMenu, "ButtonMenu");
+                    SortAddRows(dynObj.ButtonTraining, "ButtonTraining");
+                    SortAddRows(dynObj.ButtonViewer, "ButtonViewer");
+                    SortAddRows(dynObj.File, "File");
+                    SortAddRows(dynObj.Mode, "Mode");
+                    SortAddRows(dynObj.Training, "Training");
+                    SortAddRows(dynObj.Viewer, "Viewer");
+
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        dataGridView.DataSource = dataTable;
+                        dataGridView.Columns["num"].Visible = false;
+                        dataGridView.Columns["userName"].HeaderText = "User Name";
+                        dataGridView.Columns["time"].HeaderText = "Date";
+                        dataGridView.Columns["tag"].HeaderText = "TAG";
+                        dataGridView.Columns["filename"].HeaderText = "File Name";
+                        dataGridView.Columns["location"].HeaderText = "Location";
+                        dataGridView.Columns["mode"].HeaderText = "Mode";
+                        dataGridView.Columns["text"].HeaderText = "Text";
+                        dataGridView.Columns["wpm"].HeaderText = "WPM";
+                        dataGridView.Columns["delete"].HeaderText = "Delete";
+                        dataGridView.Columns["gap"].HeaderText = "Gap";
+                        dataGridView.Columns["activityName"].HeaderText = "Activity Name";
+                        dataGridView.Columns["pushButton"].HeaderText = "Push Button";
+                        dataGridView.Columns["value"].HeaderText = "Value";
+
+                        dataTable.DefaultView.Sort = "time";
+                        dataGridView.ClearSelection();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void AddRows(dynamic rows, string st)
+        {
+            bool flag = false;
+            foreach (var row in rows)
+            {
+                string bt1 = Convert.ToString(row);
+                string[] RowData = Regex.Split(bt1.Replace("{", "").Replace("}", ""), "\",");
+                DataRow nr = dataTable.NewRow();
+
+                foreach (string rowData in RowData)
+                {
+                    flag = false;
+                    try
+                    {
+                        int idx = rowData.IndexOf(":");
+                        int idx2 = 0;
+
+                        if (rowData.StartsWith("\"-K"))
+                        {
+                            idx2 = idx;
+                            idx += rowData.Substring(idx + 1).IndexOf(":");
+                        }
+                        string RowColumns = rowData.Substring(0, idx - 1).Replace("\"", "").Trim();
+                        if (idx2 != 0)
+                        {
+                            RowColumns = rowData.Substring(idx2 + 7, rowData.Substring(idx2 + 7).IndexOf(":") - 1);
+                        }
+
+                        string RowDataString = rowData.Substring(idx + 2).Replace("\"", "");
+                        if (RowDataString.Contains("\r") || RowDataString.Contains("\n"))
+                        {
+                            RowDataString = RowDataString.Replace("\r", "");
+                            RowDataString = RowDataString.Replace("\n", "");
+                        }
+                        nr[RowColumns] = RowDataString;
+                        nr["tag"] = st;
+
+                        if (name == null || (!name.Contains(RowDataString)) && RowColumns.Equals("userName"))
+                        {
+                            name.Add(RowDataString);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                }
+                if (!flag)
+                {
+                    dataTable.Rows.Add(nr);
+                }
+            }
+        }
+
+        private void SortAddRows(dynamic rows, string st)
+        {
+            bool flag = false;
+            foreach (var row in rows)
+            {
+                string bt1 = Convert.ToString(row);
+                string[] RowData = Regex.Split(bt1.Replace("{", "").Replace("}", ""), "\",");
+                DataRow nr = dataTable.NewRow();
+
+                foreach (string rowData in RowData)
+                {
+                    try
+                    {
+                        int idx = rowData.IndexOf(":");
+                        int idx2 = 0;
+
+                        if (rowData.StartsWith("\"-K"))
+                        {
+                            idx2 = idx;
+                            idx += rowData.Substring(idx + 1).IndexOf(":");
+                        }
+
+                        string RowColumns = rowData.Substring(0, idx - 1).Replace("\"", "").Trim();
+                        if (idx2 != 0)
+                        {
+                            RowColumns = rowData.Substring(idx2 + 7, rowData.Substring(idx2 + 7).IndexOf(":") - 1);
+                        }
+
+                        string RowDataString = rowData.Substring(idx + 2).Replace("\"", "");
+
+                        if (RowDataString.Contains(comboBox1.SelectedItem.ToString()))
+                        {
+                            flag = true;
+                        }
+
+                        nr[RowColumns] = RowDataString;
+                        nr["tag"] = st;
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                }
+                if (flag)
+                {
+                    dataTable.Rows.Add(nr);
+                    flag = false;
+                }
+            }
+        }
+
+        private void ComboBoxSet()
+        {
+            foreach (var cb in name)
+            {
+                comboBox1.Items.Add(cb);
+            }
+        }
+
+        private void originToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            ConvertJsonToDataTable();
+            comboBox1.Visible = false;
+        }
+
+        private void sortingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            ComboBoxSet();
+            comboBox1.Visible = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SortTable();
+        }
+
+        private void tAGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form = new JsonParsing.Form2();
+            form.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XLWorkbook wb = new XLWorkbook();
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Title = "Please select a storage path";
+            dialog.OverwritePrompt = true;
+            dialog.Filter = "Excel 파일 (*.xlsx) | *.xlsx";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                wb.Worksheets.Add(dataTable,"Log Data");
+                wb.SaveAs(dialog.FileName);
+                MessageBox.Show("SAVE!!");
+            }
+            
+        }
+    }
+}
